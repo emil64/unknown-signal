ec49db1d819ca282778b1d7fd2f18bf75550c6b2
emil64 <emil@bratianu.science>
2020-03-12 10:52:02 +0000

    much much better code + threshold

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def cross_validation(x_, y_):
+def split_train_test(x_, y_):
     x = x_
     y = y_
     vx = np.array([x[0], x[4], x[9], x[14], x[19]])
@@ -13,122 +13,147 @@ def cross_validation(x_, y_):
     return nx, ny, vx, vy
 
 
-def solve_poly(x, y):
-    (nx, ny, vx, vy) = cross_validation(x, y)
-    error = 10000000000001
-    c_min = np.zeros(len(x))
-    for i in range(2, len(nx)+1):
-        c = least_squares_poly(nx, ny, i)
-        if calc_error(vy, calc_y_hat_poly(c, vx)) < error:
-            error = calc_error(vy, calc_y_hat_poly(c, vx))
-            c_min = c
-    # plot_poly(c_min, x)
-    return calc_error(y, calc_y_hat_poly(c_min, x)), c_min
+class Function:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.error = 0
+        self.a = np.zeros(len(self.x))
+
+    def solve(self):
+        return self.error
+
+    def plot(self):
+        self.error = 0
 
 
-def solve_sin(x, y):
-    c = least_squares_sin(x, y)
-    return calc_error(y, calc_y_hat_sin(c, x)), c
+class Poly(Function):
+
+    def solve(self):
+        (nx, ny, vx, vy) = split_train_test(self.x, self.y)
+        error = 10000000000001
+        c_min = np.zeros(len(self.x))
+        for i in range(2, len(nx) + 1):
+            c = self.least_squares(nx, ny, i)
+            if calc_error(vy, self.calc_y_hat(c, vx)) < error:
+                error = calc_error(vy, self.calc_y_hat(c, vx))
+                c_min = c
+        # plot_poly(c_min, x)
+        self.a = c_min
+        self.error = calc_error(self.y, self.calc_y_hat(c_min, self.x))
+        return self.error
+
+    @staticmethod
+    def least_squares(x, y, g):
+        x = x.reshape((len(x), 1))
+        x_ = np.ones((len(x), g))
+        x_[:, 1] = x[:, 0]
+
+        x_copy = x.copy()
+        for p in range(2, g):
+            for i in range(0, len(x)):
+                x_copy[i] = x_copy.item(i) * x[i]
+            x_[:, p] = x_copy[:, 0]
+
+        xt = np.transpose(x_)
+        a = np.dot(np.dot(np.linalg.inv(np.dot(xt, x_)), xt), y)
+        return a
+
+    @staticmethod
+    def calc_y_hat(a, x):
+        return np.polyval(np.flip(a), x)
+
+    def plot(self):
+        g = len(self.a)
+        print(g - 1)
+        x_new = np.linspace(self.x.min(), self.x.max(), 1000)
+        y_new = np.zeros(len(x_new))
+        for i in range(0, g):
+            y_new += (self.a[i] * (x_new ** i))
+        ax.plot(x_new, y_new, '-r')
+        # ax.scatter(x, calc_y_hat_poly(a, x), c='b', s=10)
 
 
-def solve_exp(x, y):
-    c = least_squares_exp(x, y)
-    return calc_error(y, calc_y_hat_exp(c, x)), c
+class Sine(Function):
+
+    def solve(self):
+        self.a = self.least_squares(self.x, self.y)
+        self.error = calc_error(self.y, self.calc_y_hat(self.a, self.x))
+        return self.error
+
+    @staticmethod
+    def least_squares(x, y):
+        x = x.reshape((len(x), 1))
+        x_ = np.ones((len(x), 2))
+
+        x_copy = np.sin(x)
+        x_[:, 1] = x_copy[:, 0]
+
+        xt = np.transpose(x_)
+        a = np.dot(np.dot(np.linalg.inv(np.dot(xt, x_)), xt), y)
+        return a
+
+    @staticmethod
+    def calc_y_hat(a, x):
+        return a[0] + a[1] * np.sin(x)
+
+    def plot(self):
+        x_new = np.linspace(self.x.min(), self.x.max(), 1000)
+        y_new = self.calc_y_hat(self.a, x_new)
+        ax.plot(x_new, y_new, '-b')
 
 
-def least_squares_exp(x, y):
-    x = x.reshape((len(x), 1))
-    x_ = np.ones((len(x), 2))
+class Exp(Function):
 
-    x_copy = np.exp(x)
-    x_[:, 1] = x_copy[:, 0]
+    def solve_exp(self):
+        self.a = self.least_squares(self.x, self.y)
+        return calc_error(self.y, self.calc_y_hat(self.a, self.x))
 
-    xt = np.transpose(x_)
-    a = np.dot(np.dot(np.linalg.inv(np.dot(xt, x_)), xt), y)
-    return a
+    @staticmethod
+    def least_squares(x, y):
+        x = x.reshape((len(x), 1))
+        x_ = np.ones((len(x), 2))
 
+        x_copy = np.exp(x)
+        x_[:, 1] = x_copy[:, 0]
 
-def least_squares_sin(x, y):
-    x = x.reshape((len(x), 1))
-    x_ = np.ones((len(x), 2))
+        xt = np.transpose(x_)
+        a = np.dot(np.dot(np.linalg.inv(np.dot(xt, x_)), xt), y)
+        return a
 
-    x_copy = np.sin(x)
-    x_[:, 1] = x_copy[:, 0]
+    @staticmethod
+    def calc_y_hat(a, x):
+        return a[0] + a[1] * np.sin(x)
 
-    xt = np.transpose(x_)
-    a = np.dot(np.dot(np.linalg.inv(np.dot(xt, x_)), xt), y)
-    return a
-
-
-def least_squares_poly(x, y, g):
-    x = x.reshape((len(x), 1))
-    x_ = np.ones((len(x), g))
-    x_[:, 1] = x[:, 0]
-
-    x_copy = x.copy()
-    for p in range(2, g):
-        for i in range(0, len(x)):
-            x_copy[i] = x_copy.item(i) * x[i]
-        x_[:, p] = x_copy[:, 0]
-
-    xt = np.transpose(x_)
-    a = np.dot(np.dot(np.linalg.inv(np.dot(xt, x_)), xt), y)
-    return a
+    def plot(self):
+        x_new = np.linspace(self.x.min(), self.x.max(), 1000)
+        y_new = self.calc_y_hat(self.a, x_new)
+        ax.plot(x_new, y_new, '-y')
 
 
 def solve(x, y):
+    poly = Poly(x, y)
+    poly.solve()
 
-    (error_poly, a_poly) = solve_poly(x, y)
-    (error_sin, a_sin) = solve_sin(x, y)
-    (error_exp, a_exp) = solve_exp(x, y)
-    if error_poly < error_sin and error_poly < error_exp:
-        error = error_poly
-        plot_poly(a_poly, x)
-    elif error_sin < error_poly and error_sin < error_exp:
-        print('sin')
-        plot_sin(a_sin, x)
-        error = error_sin
-    else:
-        print('exp')
-        plot_exp(a_exp, x)
-        error = error_exp
+    sin = Sine(x, y)
+    error_sin = sin.solve()
+
+    exp = Exp(x, y)
+    error_exp = exp.solve_exp()
+
+    threshold = 25
+
+    min_f = poly
+    if error_sin < min_f.error and abs(error_sin - min_f.error) > threshold:
+        print(abs(error_sin - min_f.error))
+        min_f = sin
+    elif error_exp < min_f.error and abs(error_exp - min_f.error) > threshold:
+        min_f = exp
+
+    error = min_f.error
+    min_f.plot()
 
     return error
-
-
-def plot_poly(a, x):
-    g = len(a)
-    print(g-1)
-    x_new = np.linspace(x.min(), x.max(), 1000)
-    y_new = np.zeros(len(x_new))
-    for i in range(0, g):
-        y_new += (a[i] * (x_new ** i))
-    ax.plot(x_new, y_new, '-r')
-    # ax.scatter(x, calc_y_hat_poly(a, x), c='b', s=10)
-
-
-def plot_sin(a, x):
-    x_new = np.linspace(x.min(), x.max(), 1000)
-    y_new = calc_y_hat_sin(a, x_new)
-    ax.plot(x_new, y_new, '-b')
-
-
-def plot_exp(a, x):
-    x_new = np.linspace(x.min(), x.max(), 1000)
-    y_new = calc_y_hat_exp(a, x_new)
-    ax.plot(x_new, y_new, '-y')
-
-
-def calc_y_hat_poly(a, x):
-    return np.polyval(np.flip(a), x)
-
-
-def calc_y_hat_sin(a, x):
-    return a[0] + a[1]*np.sin(x)
-
-
-def calc_y_hat_exp(a, x):
-    return a[0] + a[1] * np.sin(x)
 
 
 def calc_error(y, y_hat):
@@ -148,3 +173,5 @@ fig, ax = plt.subplots()
 xs, ys = u.load_points_from_file('train_data/adv_3.csv')
 split_data(xs, ys)
 u.view_data_segments(xs, ys)
+
+# todo implement cos, log
